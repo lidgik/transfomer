25db7aaf2927d996ab8ccbd016c59e9e07a70823
baijiayong <jiayongbai@gmail.com>
2014-07-16 18:23:50 +0800

    增加deploy脚本

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir $CATALINA_HOME/webapps/transformer
+
+cp -r src/main/webapp/* $CATALINA_HOME/webapps/transformer
+
+cp -r target/classes $CATALINA_HOME/webapps/transformer/WEB-INF
