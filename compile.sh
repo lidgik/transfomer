d37e618d5866ffd23a783e2e240827d3705216b1
baijiayong <jiayongbai@gmail.com>
2014-07-16 19:25:37 +0800

    建立数据库连接并在compile中引入所需包

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 
 mkdir target/classes
 
-javac -d target/classes -classpath $CATALINA_HOME/lib/servlet-api.jar src/main/java/com/bodejidi/ContactServlet.java
+javac -d target/classes -classpath "$CATALINA_HOME/lib/servlet-api.jar;lib/mysql-connector-java-3.1.14-bin.jar" src/main/java/com/bodejidi/ContactServlet.java
 
 
 
--- src/main/java/com/bodejidi/ContactServlet.java
+++ src/main/java/com/bodejidi/ContactServlet.java
@@ -6,10 +6,26 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class ContactServlet extends HttpServlet{
     public void doGet(HttpServletRequest req, HttpServletResponse resp) 	throws IOException, ServletException{
     
     	resp.getWriter().println(req.getParameter("name"));
         
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex){}
+        
+        try{
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=");
+            resp.getWriter().println(connection);
+            connection.close();
+        } catch(SQLException sqle){
+            resp.getWriter().println("Cannot connect to DB.");
+            resp.getWriter().println(sqle.getMessage());
+            sqle.printStackTrace();
+        }
     }
 }
