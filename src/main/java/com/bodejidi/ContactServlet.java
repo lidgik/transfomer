31ae625f0f4980a1c717f0efe926d2b6bdb7b267
baijiayong <jiayongbai@gmail.com>
2014-07-18 10:12:47 +0800

    查找所有联系人

--- src/main/java/com/bodejidi/ContactServlet.java
+++ src/main/java/com/bodejidi/ContactServlet.java
@@ -9,11 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 public class ContactServlet extends HttpServlet{
     public void doGet(HttpServletRequest req, HttpServletResponse resp)throws IOException, ServletException{
     
     	resp.getWriter().println(req.getParameter("name"));
+        String sql = "select * from contact";
         
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+            
         try{
             Class.forName("com.mysql.jdbc.Driver").newInstance();
         } catch (Exception ex)
@@ -22,13 +30,43 @@ public class ContactServlet extends HttpServlet{
         }
         
         try{
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root" + "&password=");
-            resp.getWriter().println(connection);
-            connection.close();
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root" + "&password=");
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            while (rs.next()){
+                resp.getWriter().println(rs.getInt("id"));
+                resp.getWriter().println(rs.getString("name"));
+                resp.getWriter().println(rs.getString("mobile"));
+                resp.getWriter().println(rs.getString("office_address"));
+            }
         } catch(SQLException sqle){
             resp.getWriter().println("Cannot connect to DB.");
             resp.getWriter().println(sqle.getMessage());
             sqle.printStackTrace();
+        }
+        
+        if(rs != null){
+            try{
+                rs.close();
+            }catch(SQLException sqle){
+                //ignore;
+            }
+        }
+        
+        if(stmt != null){
+            try{
+                stmt.close();
+            }catch(SQLException sqle){
+                //ignore;
+            }
+        }
+        
+        if(conn != null){
+            try{
+                conn.close();
+            }catch(SQLException sqle){
+                //ignore;
+            }
         }
     }
 }
