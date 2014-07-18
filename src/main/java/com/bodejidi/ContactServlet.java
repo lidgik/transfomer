0da24030c271aec40a83ad8de2337c84d11bbcf2
baijiayong <jiayongbai@gmail.com>
2014-07-18 11:25:25 +0800

    添加查找所有联系人

--- src/main/java/com/bodejidi/ContactServlet.java
+++ src/main/java/com/bodejidi/ContactServlet.java
@@ -15,17 +15,67 @@ import java.sql.ResultSet;
 public class ContactServlet extends HttpServlet{
     public void doGet(HttpServletRequest req, HttpServletResponse resp)throws IOException, ServletException{
         String sql = null;
-    	
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+            	
         if ((req.getParameter("name") == null) || (req.getParameter("name") == "")){
+            sql = "select * from contact";
             resp.getWriter().println("get all contacts!");
+          
+            try{
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+            } catch (Exception ex)
+            {
+                //ignore;
+            }
+            
+            try{
+                conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root" + "&password=");
+                stmt = conn.createStatement();
+                rs = stmt.executeQuery(sql);
+                
+                while (rs.next()){
+                    resp.getWriter().println(rs.getInt("id"));
+                    resp.getWriter().println(rs.getString("name"));
+                    resp.getWriter().println(rs.getString("mobile"));
+                    resp.getWriter().println(rs.getString("office_address"));
+                }
+          
+            } catch(SQLException sqle){
+                resp.getWriter().println("Cannot connect to DB.");
+                resp.getWriter().println(sqle.getMessage());
+                sqle.printStackTrace();
+            }
+            
+            if(rs != null){
+                try{
+                    rs.close();
+                }catch(SQLException sqle){
+                    //ignore;
+                }
+            }
+            
+            if(stmt != null){
+                try{
+                    stmt.close();
+                }catch(SQLException sqle){
+                    //ignore;
+                }
+            }
+            
+            if(conn != null){
+                try{
+                    conn.close();
+                }catch(SQLException sqle){
+                    //ignore;
+                }
+            }
         }else{
             sql = "select * from contact where name ='" + req.getParameter("name") + "'";
             resp.getWriter().println(req.getParameter("name"));
             
-            Connection conn = null;
-            Statement stmt = null;
-            ResultSet rs = null;
-                
+
             try{
                 Class.forName("com.mysql.jdbc.Driver").newInstance();
             } catch (Exception ex)
