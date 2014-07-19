629288b8238b7e022cece4a36d794c24effcae4d
baijiayong <jiayongbai@gmail.com>
2014-07-19 16:50:52 +0800

    提取出DatabaseManager到另一个文件中

--- src/main/java/com/bodejidi/ContactServlet.java
+++ src/main/java/com/bodejidi/ContactServlet.java
@@ -6,10 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.ResultSet;
 
 import java.util.List;
@@ -94,53 +91,3 @@ public class ContactServlet extends HttpServlet{
     }
 }
 
-class DatabaseManager{
-    Connection conn = null;
-    Statement stmt = null;
-    ResultSet rs = null;
-    
-    public DatabaseManager createDatebaseConnectionAndExecute(String sql){
-        try{
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-        } catch (Exception ex)
-        {
-            //ignore;
-        }
-        
-        try{
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root" + "&password=");
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery(sql);
-        } catch(SQLException sqle){
-            sqle.printStackTrace();
-        }
-        
-        return this;
-    }
-    
-    public void close(){
-        if(rs != null){
-            try{
-                rs.close();
-            }catch(SQLException sqle){
-                //ignore;
-            }
-        }
-        
-        if(stmt != null){
-            try{
-                stmt.close();
-            }catch(SQLException sqle){
-                //ignore;
-            }
-        }
-        
-        if(conn != null){
-            try{
-                conn.close();
-            }catch(SQLException sqle){
-                //ignore;
-            }
-        }
-    }
-}
