10f6168eab14171417509885946c9eca6ecb5353
baijiayong <jiayongbai@gmail.com>
2014-07-19 17:47:26 +0800

    CreateServlet获取表单参数并打印

--- src/main/java/com/bodejidi/ContactCreateServlet.java
+++ src/main/java/com/bodejidi/ContactCreateServlet.java
@@ -18,7 +18,15 @@ public class ContactCreateServlet extends HttpServlet{
     }
     
     public void doPost(HttpServletRequest req, HttpServletResponse resp)throws IOException, ServletException{
-       resp.getWriter().println("DoPost :");
+            resp.getWriter().println("Name: " + req.getParameter("name"));
+            resp.getWriter().println("Mobile: " + req.getParameter("mobile"));
+            resp.getWriter().println("Vpmn: " + req.getParameter("vpmn"));
+            resp.getWriter().println("Email: " + req.getParameter("email"));
+            resp.getWriter().println("HomeAddress: " + req.getParameter("homeAddress"));
+            resp.getWriter().println("OfficeAddress: " + req.getParameter("officaAddress"));
+            resp.getWriter().println("Memo: " + req.getParameter("memo"));
+            resp.getWriter().println("Job: " + req.getParameter("job"));
+            resp.getWriter().println("JobLevel: " + req.getParameter("jobLevel"));
     }
     
 }
