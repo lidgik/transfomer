212fc569800e477cb301cef14ca84a3d3cebe9ac
lidgik <dbwen123@gmail.com>
2014-07-21 10:40:10 +0800

    连接数据库

--- src/main/java/com/bodejidi/GroupServlet.java
+++ src/main/java/com/bodejidi/GroupServlet.java
@@ -9,10 +9,25 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.ArrayList;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class GroupServlet extends HttpServlet{
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
 		throws IOException, ServletException{
 		resp.getWriter().println("Group servlet!");
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch(Exception e) {}
+
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&&password=");
+			resp.getWriter().println(conn);
+			conn.close();
+		} catch(SQLException sqle) {
+			sqle.printStackTrace();
+		}
 	}    
 }
